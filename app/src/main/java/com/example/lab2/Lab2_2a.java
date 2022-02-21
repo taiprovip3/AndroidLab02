6c3841e65a4f0b8bd060a85b10e42024d00a8514
taiproduaxe <taito1doraemon@gmail.com>
2022-02-21 14:50:47 +0700

    done : lab2 2a,2b chua xong 2c

---
+++ app/src/main/java/com/example/lab2/Lab2_2a.java
@@ -0,0 +1,25 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Lab2_2a extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button btn;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2_2a);
+        btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Lab2_2a.this, "Ban da click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
